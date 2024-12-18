2e03bffd01217b9e8521f9b89773fd689b9f18fa
G33161842 <42005585+G33161842@users.noreply.github.com>
2024-12-18 22:41:42 +0800

    Add files via upload

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -2,25 +2,35 @@
 #include <ESP8266HTTPClient.h>
 #include <DHT.h>
 #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 #define DHTPIN D2
-#define DHTTYPE DHT11
+#define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const char* ssid[] = {"applepie", "CHANG"};
 const char* password[] = {"000010000", "063316756"};
 const int wifiNetworks = 2;
 
-const char* serverUrl = "https://3.1.140.221:10011/upload.php";
+const char* serverUrl = "https://3.1.140.221:10001/upload.php";
 
 void setup() {
   Serial.begin(115200);
 
+   Wire.begin(D3, D4);
+
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing...");
+  
   bool connected = false;
   
   for (int i = 0; i < wifiNetworks; i++) {
-    Serial.print("try to connect wifi:");
+    Serial.print("try to connect wifi：");
     Serial.println(ssid[i]);
 
     WiFi.begin(ssid[i], password[i]);
@@ -36,6 +46,9 @@ void setup() {
       Serial.println("");
       Serial.print("connect ");
       Serial.println(ssid[i]);
+      lcd.setCursor(0, 1);
+      lcd.print("WiFi: ");
+      lcd.print(ssid[i]);
       connected = true;
       break;  // 成功連接後退出循環
     } else {
@@ -46,6 +59,8 @@ void setup() {
   
   if (!connected) {
     Serial.println("can't connect any wifi！");
+    lcd.setCursor(0, 1);
+    lcd.print("WiFi failed!");
     return;
   }
 
@@ -68,12 +83,16 @@ void loop() {
       return;
     }
 
-    Serial.print("Temp: ");
-    Serial.print(temp);
-    Serial.print("C ");
-    Serial.print("Humidity: ");
-    Serial.print(humidity);
-    Serial.println("%");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temp);
+    lcd.print("C");
+    
+    lcd.setCursor(0, 1);
+    lcd.print("Humidity: ");
+    lcd.print(humidity);
+    lcd.print("%");
 
     String postData = "temperature=" + String(temp) + "&humidity=" + String(humidity);
     int httpResponseCode = http.POST(postData);
